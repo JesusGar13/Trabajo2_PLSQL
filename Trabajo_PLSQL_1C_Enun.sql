c9d40392b0b640b0d23906b2af901a6943242bf6
JesusGar13 <jgb1007@alu.ubu.es>
2024-04-09 16:54:25 +0200

    Update Trabajo_PLSQL_1C_Enun.sql
    Se añaden los test para el caso 4 y 5

--- Trabajo_PLSQL_1C_Enun.sql
+++ Trabajo_PLSQL_1C_Enun.sql
@@ -246,15 +246,25 @@ begin
   end;
   
 
-  --caso 4 Cliente inexistente  
+  -- Caso 4: Cliente inexistente  
   begin
     inicializa_test;
+    realizar_reserva('12345678Z', 'concierto_la_moda', date '2023-6-27');
+    dbms_output.put_line('Caso 4: Reserva no realizada, cliente inexistente');
+  exception
+    when others then
+        dbms_output.put_line('Caso 4: Falla la reserva');
   end;
-  
-  --caso 5 El cliente no tiene saldo suficiente
-  begin
+
+-- Caso 5: El cliente no tiene saldo suficiente
+begin
     inicializa_test;
-  end;
+    realizar_reserva('11111111B', 'concierto_la_moda', date '2023-6-27');
+    dbms_output.put_line('Caso 5: Reserva no realizada, saldo insuficiente');
+exception
+    when others then
+        dbms_output.put_line('Caso 5: Falla la reserva');
+end;
 
   
 end;
